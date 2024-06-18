05981c233df4c97c10c7f5ad00503ab268f5b606
Vinicius Caua <vinipg1@hotmail.com>
2024-06-18 12:27:19 -0300

    feat: finalizando desafio

---
+++ Iphone/src/telefone/Iphone.java
@@ -0,0 +1,44 @@
+package telefone;
+
+import interfaces.AparelhoTelefonico;
+import interfaces.NavegadorNaInternet;
+import interfaces.ReprodutorMusical;
+
+public class Iphone implements ReprodutorMusical, AparelhoTelefonico, NavegadorNaInternet {
+
+    public void ligar(String numero) {
+
+    }
+
+    public void atender() {
+
+    }
+
+    public void iniciarCorreioVoz() {
+
+    }
+
+    public void exibirPagina(String url) {
+
+    }
+
+    public void adicionarNovaAba() {
+
+    }
+
+    public void atualizarPagina() {
+
+    }
+
+    public void tocar() {
+
+    }
+
+    public void pausar() {
+
+    }
+
+    public void selecionarMusica(String musica) {
+
+    }
+}
